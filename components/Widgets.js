e19ee843f69fe6c8378d02e9b93d654d6dc78b2c
Rodrigo <rodrigomp88@gmail.com>
2022-09-12 16:55:12 -0300

    delete jsonpeake

--- components/Widgets.js
+++ components/Widgets.js
@@ -17,38 +17,38 @@ function Widgets({ trendingResults, followResults }) {
       </div>
       <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
         <h4 className="font-bold text-xl px-4">Que está pasando?</h4>
-        {trendingResults.map((result, index) => (
+        {/* {trendingResults.map((result, index) => (
           <Trending key={index} result={result} />
-        ))}
+        ))} */}
         <button className="hover:gb-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
           Ver más
         </button>
       </div>
       <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
         <h4 className="font-bold text-xl px-4">Sugerencia para seguir</h4>
-        {followResults.map((result, index) => (
-          <div
-            key={index}
-            className="hover:gb-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center"
-          >
-            <Image
-              src={result.userImg}
-              width={50}
-              height={50}
-              objectFit="cover"
-              className="rounded-full"
-            />
-            <div className="ml-4 leading-5 group">
-              <h4 className="font-bold group-hover:underline">
-                {result.username}
-              </h4>
-              <h5 className="text-gray-500 text-[15px]">{result.tag}</h5>
-            </div>
-            <button className="ml-auto bg-white text-black rounded-full text-sm py-1.5 px-3.5">
-              Seguir
-            </button>
+        {/* {followResults.map((result, index) => ( */}
+        <div
+          // key={index}
+          className="hover:gb-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center"
+        >
+          <img
+            src=""
+            width={50}
+            height={50}
+            objectFit="cover"
+            className="rounded-full"
+          />
+          <div className="ml-4 leading-5 group">
+            <h4 className="font-bold group-hover:underline">
+              {/* {result.username} */}
+            </h4>
+            <h5 className="text-gray-500 text-[15px]">{/* {result.tag} */}</h5>
           </div>
-        ))}
+          <button className="ml-auto bg-white text-black rounded-full text-sm py-1.5 px-3.5">
+            Seguir
+          </button>
+        </div>
+        {/* ))} */}
         <button className="hover:gb-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
           Ver más
         </button>
